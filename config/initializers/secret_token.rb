aa963014fc88402983b1a4e9d56e4d2999665cf7
Jebus <jebus@jebus.(none)>
2012-03-14 23:16:12 -0600

    Login/Signup set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twebb::Application.config.secret_token = '00e9af3e5c9a806b016bd2e5ece012c93a588b7e089850146bcad5808279443bdc820019e8c28169b8f70f846a7248d9405acc62792f395f235fd3a87af9f1c9'
